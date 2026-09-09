5523aeaffbfa3ed60686ee5a2f373488321238cf
pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-09-09 08:52:49 -0300

    add - mudei para php algum arquivos e mudar de paginas funcional

--- public/usuarios_cadastrados.php
+++ public/usuarios_cadastrados.php
@@ -24,24 +24,34 @@
 
     <div class="layout">
 
-        <aside class="menu-lateral">
-            <a href="tela_inicial.html" class="item">
-                <img src="../assets/icons/dashboard_branco.svg" alt=""> Dashboard
-            </a>
-            <a href="gerenciar_sensores.html" class="item">
-                <img src="../assets/icons/sensor_branco.svg" alt=""> Sensores
-            </a>
-            <a href="relatorios.html" class="item">
-                <img src="../assets/icons/relatorio_branco.svg" alt=""> Relatórios
-            </a>
-            <a href="tela_de_cadastro.html" class="item">
-                <img src="../assets/icons/cadastrar_branco.svg" alt=""> Cadastrar Usuários
-            </a>
-            <a href="usuarios_cadastrados.html" class="item ativo">
-                <img src="../assets/icons/usuarios_preto.svg" alt=""> Usuários Cadastrados
-            </a>
-        </aside>
+       <aside class="menu-lateral">
 
+        <a href="tela_inicial.php" class="item ativo">
+            <img src="../assets/icons/dashboard_preto.svg" alt="">
+            Dashboard
+        </a>
+
+        <a href="gerenciar_sensores.php" class="item">
+            <img src="../assets/icons/sensor_branco.svg" alt="">
+            Sensores
+        </a>
+
+        <a href="relatorios.php" class="item">
+            <img src="../assets/icons/relatorio_branco.svg" alt="">
+            Relatórios
+        </a>
+
+        <a href="tela_de_cadastro.php" class="item">
+            <img src="../assets/icons/cadastrar_branco.svg" alt="">
+            Cadastrar Usuários
+        </a>
+
+        <a href="usuarios_cadastrados.php" class="item">
+            <img src="../assets/icons/usuarios_branco.svg" alt="">
+            Usuários Cadastrados
+        </a>
+
+    </aside>
         <main class="conteudo">
 
             <DIV class="planilha_dashboard">
