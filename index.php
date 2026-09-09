50883608405d283a6f57837ce17b78753552b311
pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-09-09 08:00:02 -0300

    add - index modificado, tela de cadastro tambem modificada. ambas funcionam com o banco de dados

--- index.php
+++ index.php
@@ -145,15 +145,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 </div>
 
-                <div class="text-center mt-3">
-
-                    <span>Não tem login?</span>
-
-                    <a href="tela_de_cadastro.php" class="text-decoration-none">
-                        Crie uma conta
-                    </a>
-
-                </div>
+                
 
             </form>
 
