e79a793e105929cfd0f2b14ebd01f3cdebf1302b
davi_zilz <davi_zilz@estudante.sesisenai.org.br>
2026-09-17 09:07:37 -0300

    up-

--- public/gerenciar_sensores.php
+++ public/gerenciar_sensores.php
@@ -159,7 +159,7 @@ $listaStatus = $conexao->query(
 
             <a href="rotas.php" class="item">
 
-                <img src="../assets/icons/relatorio_branco.svg" alt="">
+                <img src="../assets/icons/rotas_branco.svg" alt="">
 
                 Rotas
 
@@ -227,6 +227,15 @@ $listaStatus = $conexao->query(
                     onclick="window.location.href='cadastrar_sensores.php'">
 
                     Cadastrar
+
+                </button>
+
+
+  <button
+                    class="botao_cancelar"
+                    onclick="window.location.href='editar_sensores.php'">
+
+                    Editar
 
                 </button>
 
